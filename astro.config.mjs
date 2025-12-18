76dccdd8a650f530606fae4792d0e13087da3209
serudda <sergioruizdavila@gmail.com>
2025-12-18 15:56:24 -0500

    feat: integrate Tailwind CSS into Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [mdx(), react()],
 	output: "static",
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
